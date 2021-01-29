00e239e56bf7e0d553a5c15e53905932539be11b
오진주 <jinjuoh@hancom.com>
2021-01-29 14:03:55 +0900

    d

--- src/programmers/stackquePractice/Stock.java
+++ src/programmers/stackquePractice/Stock.java
@@ -1,11 +1,12 @@
 package programmers.stackquePractice;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Stock {
 
     public static void main(String[] args) {
-        int[] prices = new int[] {};
+        int[] prices = new int[] {1,2,3,2,3};
         Stock s = new Stock();
         int[] ret = s.solution(prices);
         System.out.println(Arrays.toString(ret));
@@ -13,6 +14,7 @@ public class Stock {
 
     public int[] solution(int[] prices) {
         int[] answer = {};
+        Stack<int[]> stack = new Stack<>();
 
         return answer;
     }
